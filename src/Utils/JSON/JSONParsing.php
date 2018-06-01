c9841558c14cd992d835e6274757b1d054c3815d
Antoine Aflalo <antoine.aflalo@zerospam.ca>
2018-06-01 11:25:27 -0400

    Static class used to parse JSON response

---
+++ src/Utils/JSON/JSONParsing.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aaflalo
+ * Date: 18-06-01
+ * Time: 11:11
+ */
+
+namespace ZEROSPAM\Framework\SDK\Utils\JSON;
+
+use Psr\Http\Message\ResponseInterface;
+
+final class JSONParsing
+{
+    private function __construct()
+    {
+    }
+
+    /**
+     * Parse the content of a response
+     *
+     * @param ResponseInterface $response
+     *
+     * @return array
+     */
+    public static function responseToJson(ResponseInterface $response)
+    {
+        $contents = $response->getBody()->getContents();
+        if (empty($contents)) {
+            return [];
+        }
+
+        return \GuzzleHttp\json_decode($contents, true);
+    }
+}
