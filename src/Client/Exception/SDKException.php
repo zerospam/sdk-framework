851226c225a96d10c394943fd973bfb3922a8833
Antoine Aflalo <antoine.aflalo@zerospam.ca>
2018-06-01 11:26:23 -0400

    First exceptions

---
+++ src/Client/Exception/SDKException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aaflalo
+ * Date: 18-06-01
+ * Time: 10:21
+ */
+
+namespace ZEROSPAM\Framework\SDK\Client\Exception;
+
+class SDKException extends \RuntimeException
+{
+
+}
---
+++ src/Client/Exception/TooManyRetriesException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aaflalo
+ * Date: 18-06-01
+ * Time: 10:55
+ */
+
+namespace ZEROSPAM\Framework\SDK\Client\Exception;
+
+class TooManyRetriesException extends SDKException
+{
+
+}
