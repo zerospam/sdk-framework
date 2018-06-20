67a2c529e4e0802fba428f6014c5488d939d03da
Antoine Aflalo <antoine.aflalo@zerospam.ca>
2018-06-20 11:37:26 -0400

    test(Collection): Improves test suite for collection

--- tests/src/Tests/Request/CollectionRequestTest.php
+++ tests/src/Tests/Request/CollectionRequestTest.php
@@ -60,11 +60,116 @@ JSON;
 
         $response = $request->getResponse();
         $this->assertCount(2, $response);
+        $this->assertArrayNotHasKey(2, $response);
         $this->assertInstanceOf(TestResponse::class, $response[0]);
         $this->assertInstanceOf(TestResponse::class, $response[1]);
         $this->assertEquals("Stefan Malzner <stefan@adlk.io>", $response[1]->author);
 
         $this->assertEquals(2, $response->getMetaData()->count);
         $this->assertEquals(2, $response->getMetaData()->perPage);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testModifyCollectionResponse()
+    {
+        $json
+            = <<<JSON
+{
+  "response": [
+    {
+      "author": "Koma",
+      "featured": false,
+      "id": "chatwork",
+      "name": "chatwork",
+      "version": "1.0.1",
+      "icons": {
+        "png": "https://cdn.franzinfra.com/recipes/dist/chatwork/src/icon.png",
+        "svg": "https://cdn.franzinfra.com/recipes/dist/chatwork/src/icon.svg"
+      }
+    },
+    {
+      "author": "Stefan Malzner <stefan@adlk.io>",
+      "featured": false,
+      "id": "ciscospark",
+      "name": "Cisco Spark",
+      "version": "1.0.0",
+      "icons": {
+        "png": "https://cdn.franzinfra.com/recipes/dist/ciscospark/src/icon.png",
+        "svg": "https://cdn.franzinfra.com/recipes/dist/ciscospark/src/icon.svg"
+      }
+    }
+  ],
+  "meta": {
+    "per_page": 2,
+    "page": 0,
+    "pages": 1,
+    "count": 2
+  }
+}
+JSON;
+
+        $client = $this->preSuccess($json);
+
+        $request = new CollectionTestRequest();
+        $client->getOAuthTestClient()->processRequest($request);
+
+        $response = $request->getResponse();
+        $this->assertCount(2, $response);
+
+        $response[0] = new \stdClass();
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testDeleteCollectionResponse()
+    {
+        $json
+            = <<<JSON
+{
+  "response": [
+    {
+      "author": "Koma",
+      "featured": false,
+      "id": "chatwork",
+      "name": "chatwork",
+      "version": "1.0.1",
+      "icons": {
+        "png": "https://cdn.franzinfra.com/recipes/dist/chatwork/src/icon.png",
+        "svg": "https://cdn.franzinfra.com/recipes/dist/chatwork/src/icon.svg"
+      }
+    },
+    {
+      "author": "Stefan Malzner <stefan@adlk.io>",
+      "featured": false,
+      "id": "ciscospark",
+      "name": "Cisco Spark",
+      "version": "1.0.0",
+      "icons": {
+        "png": "https://cdn.franzinfra.com/recipes/dist/ciscospark/src/icon.png",
+        "svg": "https://cdn.franzinfra.com/recipes/dist/ciscospark/src/icon.svg"
+      }
+    }
+  ],
+  "meta": {
+    "per_page": 2,
+    "page": 0,
+    "pages": 1,
+    "count": 2
+  }
+}
+JSON;
+
+        $client = $this->preSuccess($json);
+
+        $request = new CollectionTestRequest();
+        $client->getOAuthTestClient()->processRequest($request);
+
+        $response = $request->getResponse();
+        $this->assertCount(2, $response);
+
+        unset($response[0]);
     }
 }
